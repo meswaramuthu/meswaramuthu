b5ec9655516024bad920e0cd154a28981b807ea6
eswaramuthu <meswaramuthu@gmail.com>
2026-02-15 20:16:19 +0530

    React Components are added

--- script.js
+++
@@ -1,9 +0,0 @@
-// Get references to the hamburger and nav menu
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
-
-// Toggle the visibility of the nav menu when the hamburger is clicked
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
-
